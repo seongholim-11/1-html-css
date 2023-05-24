f0ac83fbbae1d03d6f4e687e8bd01d4f33bea5a1
seongho <damadu01@gmail.com>
2023-05-24 12:27:03 +0900

    함수

--- 03_Javescript/03.반복문/js/script1.js
+++ 03_Javescript/03.반복문/js/script1.js
@@ -1,3 +1,25 @@
-for(let i = 0; i < 100; i++){
+/* for(let i = 0; i < 100; i++){
     document.write("사랑해 <br>");
-}
+} */
+
+/* let bodyTag = document.getElementsByTagName('body')[0];
+for(let i = 0; i < 6; i++){
+    bodyTag.innerHTML += "<h1> 재미있는 자바스크립트 </h1>";
+} */
+
+/* for(let j=2; j < 10; j++){
+    document.write(j+"단 시작 <br />")
+    for(let k=1; k < 10; k++){
+        document.write(j +" X " + k + " = " + j * k + "<br />")
+    }
+    document.write("<br />")
+} */
+
+/* let bodyTag = document.getElementsByTagName('body');
+for(let i = 0; i < 6; i++){
+    bodyTag.innerHTML += "<table></table>";
+    let a = document.getElementsByTagName('table');
+    for(let j = 0; j < 6; j++){
+        a.innerHTML += "<tr><td>"+j+"</td></tr>"
+    }
+} */
