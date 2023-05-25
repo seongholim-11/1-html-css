7f6a595a4665981ac91317a6cd9679a9170ba8a5
seongho <damadu01@gmail.com>
2023-05-25 11:27:26 +0900

    dsadsadsada

---
+++ 03_Javescript/05.배열/js/script.js
@@ -0,0 +1,11 @@
+// 배열(비슷한 타입을 묶어 놓은 것을 자료구조라 하고 타입들이 있을 경우 동일한 타입을 담아놓을 수 있음)
+// 1. 배열 선언
+const arr1 = new Array();
+const arr2 = [1, 2, 3, 4];
+const arr3 = ["a", "b"];
+
+const fruits = ["사과", "체리", "오렌지", "포도"]
+console.log("🚀 ~ file: script.js:8 ~ fruits:", fruits)
+console.log("🚀 ~ file: script.js:9 ~ fruits:", fruits.length)
+console.log("🚀 ~ file: script.js:10 ~ fruits:", fruits[fruits.length-1])
+
