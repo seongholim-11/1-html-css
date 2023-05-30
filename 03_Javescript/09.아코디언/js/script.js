3a81ff434b9707cf96dd91a74cb599b697fa4f8f
seongho <damadu01@gmail.com>
2023-05-30 13:05:32 +0900

    아코디언

---
+++ 03_Javescript/09.아코디언/js/script.js
@@ -0,0 +1,37 @@
+const btn = document.getElementById('btn')
+const btn1 = document.getElementById('btn1')
+const question = document.getElementsByClassName('question')
+const heading = document.getElementsByClassName('heading')
+const body = document.getElementsByClassName('body')
+
+
+function activeBody(){
+    for(let i = 0; i < question.length; i++){
+        body[i].style.display = 'none'
+    }
+    const active = document.querySelector('.active>.body')
+    active.style.display = 'block'
+}
+
+activeBody();
+
+for(let j = 0; j<heading.length; j++){
+    heading[j].addEventListener('click', function(e){
+        for(let k = 0; k<question.length; k++){
+            question[k].classList.remove('active')
+            e.target.parentNode.classList.add('active')
+            activeBody()
+        }
+    })
+}
+
+btn.addEventListener('click', function(){
+    for(let k = 0; k<question.length; k++){
+        body[k].style.display = 'none'
+    }
+})
+btn1.addEventListener('click', function(){
+    for(let k = 0; k<question.length; k++){
+        body[k].style.display = 'block'
+    }
+})
