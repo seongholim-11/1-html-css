c76410aacc316b0af046cad3d98b0e7e81d95002
seongho <damadu01@gmail.com>
2023-06-08 15:28:58 +0900

    당구장 만들기

---
+++ 04_Jquery/01.jquery_select/06_객체조작/object/js/script2.js
@@ -0,0 +1,14 @@
+//animate
+$('.bar').animate({bottom: '-70px'},800)
+$('.ball11').delay(1000).animate({top: '360px'},700)
+$('.ball10').delay(1500).animate({top: '0px', left: '460px'},1000).animate({top: '70px', left: '450px'},1000)
+$('.ball9').delay(1500).animate({top: '0px', left: '300px'},1000).animate({top: '70px', left: '330px'},1000)
+$('.ball8').delay(1500).animate({top: '0px', left: '200px'},1000).animate({top: '70px', left: '300px'},1000)
+$('.ball7').delay(1500).animate({top: '0px', left: '50px'},1000).animate({top: '50px', left: '00px'},1000)
+$('.ball6').delay(1500).animate({top: '150px', left: '0px'},1000).animate({top: '120px', left: '30px'},1000)
+$('.ball5').delay(1500).animate({top: '120px', left: '50px'},1000).animate({top: '200px', left: '80px'},1000)
+$('.ball4').delay(1500).animate({top: '90px', left: '80px'},1000).animate({top: '230px', left: '150px'},1000)
+$('.ball3').delay(1500).animate({top: '210px', left: '80px'},1000).animate({top: '230px', left: '210px'},1000)
+$('.ball2').delay(1500).animate({top: '220px', left: '460px'},1000).animate({top: '230px', left: '400px'},1000)
+$('.ball1').delay(1500).animate({top: '250px', left: '250px'},1000).animate({top: '200px', left: '230px'},1000)
+
