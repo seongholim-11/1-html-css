89e18076f909e39d772c38a1d23c767fa493ec91
seongho <damadu01@gmail.com>
2023-06-09 16:19:37 +0900

    완성

--- 04_Jquery/02.jquery_menu/메뉴/js/script2.js
+++ 04_Jquery/02.jquery_menu/메뉴/js/script2.js
@@ -2,6 +2,7 @@ const menu = $('#nav li')
 
 
 menu.hover(function(){
+    close()
     let tg = $(this);
     let onImg = tg.find('.menuText-image>.on');
     let offImg = tg.find('.menuText-image>.off');
@@ -22,4 +23,31 @@ menu.hover(function(){
     offImg.css({display: 'block'})
 
     menuImg.stop().animate({width: 0}, 600)
+    open()
 })
+
+open()
+function open(){
+    let tg = $('#nav>li').eq(0);
+    let onImg = tg.find('.menuText-image>.on');
+    let offImg = tg.find('.menuText-image>.off');
+    let menuImg = tg.find('.menu-image');
+    let imageWidth = menuImg.find('img').innerWidth();
+
+    onImg.css({display: 'block'})
+    offImg.css({display: 'none'})
+
+    menuImg.stop().animate({width: imageWidth}, 600)
+}
+
+function close(){
+    let tg = $('#nav>li').eq(0);
+    let onImg = tg.find('.menuText-image>.on');
+    let offImg = tg.find('.menuText-image>.off');
+    let menuImg = tg.find('.menu-image');
+
+    onImg.css({display: 'none'})
+    offImg.css({display: 'block'})
+
+    menuImg.stop().animate({width: 0}, 600)
+}
