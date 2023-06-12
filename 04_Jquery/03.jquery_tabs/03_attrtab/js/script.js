b915e67934bf32d9464707510f9b8a3e24ed5ba4
seongho <damadu01@gmail.com>
2023-06-12 12:00:33 +0900

    완성

--- 04_Jquery/03.jquery_tabs/03_attrtab/js/script.js
+++ 04_Jquery/03.jquery_tabs/03_attrtab/js/script.js
@@ -4,15 +4,19 @@ const fall = $('#fallBtn a')
 const winter = $('#winterBtn a')
 let image = $('#image img')
 
-spring.click(function(){
+spring.click(function(e){
+    e.preventDefault();
     image.attr('src', './images/spring.jpg')
 })
-summer.click(function(){
+summer.click(function(e){
+    e.preventDefault();
     image.attr('src', './images/summer.jpg')
 })
-fall.click(function(){
+fall.click(function(e){
+    e.preventDefault();
     image.attr('src', './images/fall.jpg')
 })
-winter.click(function(){
+winter.click(function(e){
+    e.preventDefault();
     image.attr('src', './images/winter.jpg')
 })
