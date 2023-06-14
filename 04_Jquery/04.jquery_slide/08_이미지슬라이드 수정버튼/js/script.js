2c6606ad799f2db9a593713af74e27d5ac3fef78
seongho <damadu01@gmail.com>
2023-06-14 11:00:28 +0900

    슬라이드 완성

--- 04_Jquery/04.jquery_slide/08_이미지슬라이드 수정버튼/js/script.js
+++ 04_Jquery/04.jquery_slide/08_이미지슬라이드 수정버튼/js/script.js
@@ -1,94 +1,96 @@
-$(function(){
-	var banner=$('.slideUl>li');
-	var button=$('.buttonList>li');
-	var leftBtn=$('.btnImg>.prev'); 
-	var rightBtn=$('.btnImg>.next'); 
-	var current=0;
-	var setIntervalId;
-	
-	timer();
-	function timer(){
-		setIntervalId=setInterval(function(){
-			var prev=banner.eq(current);
-			var prevBtn=button.eq(current);
-			move(prev,0,'-100%');
-			prevBtn.removeClass('on');
-			current++;
-			if(current==banner.size()){current=0;}
-			var next=banner.eq(current);
-			var nextBtn=button.eq(current);
-			move(next,'100%',0);
-			nextBtn.addClass('on');
-		},2000);
-	}
-	function move(tg, start, end){
-		tg.css('left',start).stop().animate({left:end},500);
-	}
-	
-	$('.slide').on({mouseover:function(){
-		clearInterval(setIntervalId);
-	},mouseout:function(){
-		timer();
-	}
-	});
-	
-	/* 양쪽 화살표 */
-	rightBtn.click(function(){
-		var prev=banner.eq(current);
-		var prevBtn=button.eq(current);
-		move(prev,0,'-100%');
-		prevBtn.removeClass('on');
-		current++;
-		if(current==banner.size()){current=0;}
-		var next=banner.eq(current);
-		var nextBtn=button.eq(current);
-		move(next,'100%',0);
-		nextBtn.addClass('on');
-	});
-	leftBtn.click(function(){
-		var prev=banner.eq(current);
-		var prevBtn=button.eq(current);
-		move(prev,0,'100%');
-		prevBtn.removeClass('on');
-		current--;
-		if(current==-banner.size()){current=0;}
-		var next=banner.eq(current);
-		var nextBtn=button.eq(current);
-		move(next,'-100%',0);
-		nextBtn.addClass('on');
-	});
-	
-	//button.on({click:function(){}});
-	button.click(function(){
-		var tg=$(this);
-		var i=tg.index();
-		
-		button.removeClass('on');
-		tg.addClass('on');
-		if(current>i){
-			move2(i);
-		}else{
-			move1(i);
-		}
-		
-	});
-	
-	function move1(i){
-		if(current==i) return;// 현재 보이는 이미지가 i번째랑 같은면 종료시켜
-		var currentEl=banner.eq(current);
-		var nextEl=banner.eq(i);
-		currentEl.css("left","0").stop().animate({left:'-100%'},500);
-		nextEl.css('left','100%').stop().animate({left:0},500);
-		current=i;
-	}
-	function move2(i){
-		if(current==i) return;// 현재 보이는 이미지가 i번째랑 같은면 종료시켜
-		var currentEl=banner.eq(current);
-		var nextEl=banner.eq(i);
-		currentEl.css("left","0").stop().animate({left:'100%'},500);
-		nextEl.css('left','-100%').stop().animate({left:0},500);
-		current=i;
-	}
-	
-	
-});
+const banner = $('.slideUl li');
+const prev = $('.btnImg>.prev');
+const next = $('.btnImg>.next');
+const button = $('.buttonList li');
+
+let cnt = 0;
+let setIntervalId;
+
+// timer();
+function timer() {
+    setIntervalId = setInterval(function () {
+        let prev = banner.eq(cnt);
+        let prevBtn = button.eq(cnt);
+        move(prev, 0, '-100%')
+        prevBtn.removeClass('on');
+        cnt++;
+        if (cnt == banner.size()) {
+            cnt = 0;
+        }
+
+        let next = banner.eq(cnt);
+        let nextBtn = button.eq(cnt);
+        move(next, '100%', 0)
+        nextBtn.addClass('on');
+    }, 2000)
+}
+
+
+
+$('.slide').on({
+    mouseover: function () {
+        clearInterval(setIntervalId);
+    }, mouseout: function () {
+        // timer();
+    }
+})
+
+next.click(function () {
+    let prev = banner.eq(cnt);
+    let prevBtn = button.eq(cnt);
+    move(prev, 0, '-100%')
+    prevBtn.removeClass('on');
+    cnt++;
+    if (cnt == banner.size()) {
+        cnt = 0;
+    }
+
+    let next = banner.eq(cnt);
+    let nextBtn = button.eq(cnt);
+    move(next, '100%', 0)
+    nextBtn.addClass('on');
+})
+
+prev.click(function () {
+    let prev = banner.eq(cnt);
+    let prevBtn = button.eq(cnt);
+    move(prev, 0, '100%')
+    prevBtn.removeClass('on');
+    cnt--;
+    if (cnt == -1) {
+        cnt = 2;
+    }
+
+    let next = banner.eq(cnt);
+    let nextBtn = button.eq(cnt);
+    move(next, '-100%', 0)
+    nextBtn.addClass('on');
+})
+
+function move(tg, start, end) {
+    tg.css('left', start).stop().animate({ left: end }, 500)
+}
+
+button.click(function () {
+    let tg = $(this);
+    let i = tg.index();
+
+    button.removeClass('on');
+    tg.addClass('on')
+
+    if (cnt > i) {
+        if (cnt == i) return;
+        let cntEl = banner.eq(cnt);
+        let nextEl = banner.eq(i);
+        cntEl.css('left', 0).stop().animate({ left: '100%' }, 500);
+        nextEl.css('left', '-100%').stop().animate({ left: 0 }, 500);
+        cnt = i;
+    } else {
+        if (cnt == i) return;
+        let cntEl = banner.eq(cnt);
+        let nextEl = banner.eq(i);
+        cntEl.css('left', 0).stop().animate({ left: '-100%' }, 500);
+        nextEl.css('left', '100%').stop().animate({ left: 0 }, 500);
+        cnt = i;
+    }
+})
