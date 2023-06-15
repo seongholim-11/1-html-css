3f65c4acee0075eb8adb0d9fe44cbc655c066e39
seongho <damadu01@gmail.com>
2023-06-15 15:53:19 +0900

    완성

--- 04_Jquery/blindmask/js/script.js
+++ 04_Jquery/blindmask/js/script.js
@@ -13,17 +13,54 @@ $('.arrowPrevBt').on('click', function(){
     prevCountfn()
 })
 
-$('#section1').hover(function(){
-    clearInterval(setintervalID);
-}, function(){
-    autoplay();
-})
-
+// 자동슬라이드
 autoplay();
 function autoplay(){
     setintervalID = setInterval(nextCountfn,3000)
 }
 
+// 일시정지
+$('#section1').hover(function(){
+    clearInterval(setintervalID);
+    mouseWheelEventFn()
+}, function(){
+    autoplay();
+})
+
+$('.slide-wrap').swipe({
+    swipeLeft: function(){
+        nextCountfn()
+    },
+    swipeRight: function(){
+        prevCountfn()
+    }
+})
+
+// 마우스 휠 이벤트 함수
+function mouseWheelEventFn(){
+    $('#section1').on('mousewheel DOMMouseScroll', function(e){
+        e.preventDefault();
+        if(e.originalEvent.detail){ // 파이어폭스
+            delta = -e.originalEvent.detail * 40
+        }else if(e.originalEvent.wheelDelta){ // 파이어폭스 제외
+            delta = e.originalEvent.wheelDelta
+        }
+
+        if(t==0){
+            t = 1;
+            if(delta < 0){
+                nextCountfn()
+            }else{
+                prevCountfn()
+            }
+            setTimeout(function(){
+                t = 0
+            }, 100)
+        }
+    })
+}
+
+// 다음으로 이동
 function nextCountfn() {
     cnt++;
     if (cnt > n) { cnt = 0; }
@@ -32,6 +69,15 @@ function nextCountfn() {
     mainNextSlidefn();
 }
 
+// 이전으로 이동
+function prevCountfn() {
+    cnt--;
+    if (cnt < 0) { cnt = n; }
+    z = (cnt == n ? 0 : cnt + 1)
+    mainPrevSlidefn();
+}
+
+// 페이지 네이션
 function pageBtfn(){
     $('.pageBt').removeClass('addpageBt');
     $('.pageBt').eq(cnt).addClass('addpageBt');
@@ -78,9 +124,3 @@ function mainPrevSlidefn() {
     }
 }
 
-function prevCountfn() {
-    cnt--;
-    if (cnt < 0) { cnt = n; }
-    z = (cnt == n ? 0 : cnt + 1)
-    mainPrevSlidefn();
-}
