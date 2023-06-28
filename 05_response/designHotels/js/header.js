574285f15e5b9d8094de02ba1ca24afc0a3b3c3b
seongho <damadu01@gmail.com>
2023-06-28 15:00:52 +0900

    html css 완성

---
+++ 05_response/designHotels/js/header.js
@@ -0,0 +1,17 @@
+let t = 0;
+let winW = 0;
+const appbarBt = $('.appbarBt1')
+const mobileModalMenu = $('.mobile-modal-menu');
+const appbarCloseBt = $('.appbarCloseBt');
+const header = $('#header');
+
+appbarBt.on('click', function(){
+    mobileModalMenu.stop().animate({left: 0}, 500);
+    header.stop().animate({left: '100%'}, 500);
+
+})
+
+appbarCloseBt.on('click', function(){
+    mobileModalMenu.stop().animate({left: '-100%'}, 500);
+    header.stop().animate({left: '0'}, 500);
+})
---
+++ 05_response/designHotels/js/main.js
@@ -0,0 +1,4 @@
+let winH = $(window).innerHeight();
+let winW = $(window).innerWidth();
+
+let vidW = $('.mainVideo').innerWidth();
