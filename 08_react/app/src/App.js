bc16d40a924ebc24d3578ab4701b138d40e6d7e5
seongho <damadu01@gmail.com>
2023-07-12 16:15:31 +0900

    글 추가 삭제하기

--- 08_react/app/src/App.js
+++ 08_react/app/src/App.js
@@ -1,4 +1,4 @@
-
+/* 
 import './App.css';
 import Iteration from './Iteration';
 // import ScrollBox from './Scrollbox';
@@ -8,12 +8,31 @@ class App extends Component {
   render() {
     return (
       <div>
-{/*          <ScrollBox ref={(ref) => this.ScrollBox=ref}/>
-       <button onClick={()=>this.ScrollBox.scrollToBottom()}>맨밑으로</button> */}
+        <ScrollBox ref={(ref) => this.ScrollBox=ref}/>
+       <button onClick={()=>this.ScrollBox.scrollToBottom()}>맨밑으로</button>
        <Iteration/>
       </div>
     );
   }
 }
 
-export default App;
+export default App; */
+
+
+import './App.css';
+import Iteration from './Iteration';
+// import ScrollBox from './Scrollbox';
+import React from 'react';
+
+function App(){
+  return(
+    <div>
+    {/*          <ScrollBox ref={(ref) => this.ScrollBox=ref}/>
+           <button onClick={()=>this.ScrollBox.scrollToBottom()}>맨밑으로</button> */}
+           <Iteration/>
+          </div>
+  )
+}
+
+
+export default App
