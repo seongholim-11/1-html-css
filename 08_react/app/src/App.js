a0ef0d38079d492c2d2f0990c2e4f4d93032764c
seongho <damadu01@gmail.com>
2023-07-13 10:36:59 +0900

    파일 삭제

--- 08_react/app/src/App.js
+++ 08_react/app/src/App.js
@@ -1,38 +1,9 @@
-/* 
-import './App.css';
-import Iteration from './Iteration';
-// import ScrollBox from './Scrollbox';
-import React, { Component } from 'react';
+import React from 'react'
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <ScrollBox ref={(ref) => this.ScrollBox=ref}/>
-       <button onClick={()=>this.ScrollBox.scrollToBottom()}>맨밑으로</button>
-       <Iteration/>
-      </div>
-    );
-  }
-}
-
-export default App; */
-
-
-import './App.css';
-import Iteration from './Iteration';
-// import ScrollBox from './Scrollbox';
-import React from 'react';
-
-function App(){
-  return(
-    <div>
-    {/*          <ScrollBox ref={(ref) => this.ScrollBox=ref}/>
-           <button onClick={()=>this.ScrollBox.scrollToBottom()}>맨밑으로</button> */}
-           <Iteration/>
-          </div>
+function App() {
+  return (
+    <div>App</div>
   )
 }
-
 
 export default App
